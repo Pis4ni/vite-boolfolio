a34b7e0d85ab93fbad84a2f48c0302ec4c7b29b2
Pis4ni <pis4wrk@gmail.com>
2023-11-07 00:51:16 +0100

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world"
+    }
+  },
+  methods:{
+
+  },
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
